a0ff4bfad150f97bac35ed8867a6eda763edcb56
RafoPetrosyan <petrosyanrafo0@gmail.com>
2022-06-15 15:52:51 +0400

    R_P

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
+const getUser = () => Promise.resolve({id: 1, name: "Yauhen"});
+
+const Search = ({ value, onChange, children }) => (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input id="search" type="text" value={value} onChange={onChange} required/>
+    </div>
+)
+
 function App() {
+  const [search, setSearch] = useState("");
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+      const loadUser = async () => await getUser();
+      setUser(loadUser());
+  }, []);
+
+  const handleChange = ({ target: {value} }) => {
+    setSearch(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        { user && <h2>Logged in as {user.name}</h2> }
+        <img src="" alt="search image" />
+        <Search value={search} onChange={handleChange}>
+            Search:
+        </Search>
+        <p>Searches for {search ? search : "..."}</p>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// describe("App", () => {
+//   it("renders App component", () => {
+//     render(<App />);
+//     expect(screen.getByText(/Search:/i)).toBeInTheDocument();
+//     expect(screen.getByRole('textbox')).toBeInTheDocument();
+//     expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
+//     expect(screen.getByPlaceholderText('search text...')).toBeInTheDocument();
+//     expect(screen.getByAltText("search image")).toBeInTheDocument();
+//     expect(screen.getByDisplayValue("")).toBeInTheDocument();
+//   });
+// });
+
+describe("App", () => {
+  it("renders App component", async () => {
+    render(<App />);
+    // expect(screen.queryByText(/Searchs for React/i)).toBeNull();
+    expect(screen.queryByText(/Logged in as /i)).toBeNull();
+    screen.debug();
+    expect(await screen.findByText(/Logged in as/i)).toBeInTheDocument();
+    screen.debug();
+  });
 });
