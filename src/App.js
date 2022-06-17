1059719f57400ff0329b28e5e11bdfb222508df5
RafoPetrosyan <petrosyanrafo0@gmail.com>
2022-06-17 17:02:39 +0400

    R_P

--- src/App.js
+++ src/App.js
@@ -1,38 +1,76 @@
-import React, {useEffect, useState} from "react";
-import './App.css';
+// import React, {useEffect, useState} from "react";
+// import './App.css';
+//
+// const getUser = () => Promise.resolve({id: 1, name: "Yauhen"});
+//
+// const Search = ({ value, onChange, children }) => (
+//     <div>
+//       <label htmlFor="search">{children}</label>
+//       <input id="search" type="text" value={value} onChange={onChange}/>
+//     </div>
+// )
+//
+// function App() {
+//   const [search, setSearch] = useState("");
+//   const [user, setUser] = useState("");
+//
+//   useEffect(() => {
+//       const loadUser = async () => await getUser();
+//       setUser(loadUser());
+//   }, []);
+//
+//   const handleChange = ({ target: {value} }) => {
+//     setSearch(value);
+//   };
+//
+//   return (
+//     <div className="App">
+//         { user && <h2>Logged in as {user.name}</h2> }
+//         <img className="image" src="" alt="search image" />
+//         <Search value={search} onChange={handleChange}>
+//             Search:
+//         </Search>
+//         <p>Searches for {search ? search : "..."}</p>
+//     </div>
+//   );
+// }
+//
+// export default App;
 
-const getUser = () => Promise.resolve({id: 1, name: "Yauhen"});
 
-const Search = ({ value, onChange, children }) => (
-    <div>
-      <label htmlFor="search">{children}</label>
-      <input id="search" type="text" value={value} onChange={onChange}/>
-    </div>
-)
+import React, {useState} from 'react';
+import axios from "axios";
+const URL = "http://hn.algolia.com/api/v1/search";
 
-function App() {
-  const [search, setSearch] = useState("");
-  const [user, setUser] = useState("");
+const App = () => {
+    const [news, setNews] = useState([]);
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-      const loadUser = async () => await getUser();
-      setUser(loadUser());
-  }, []);
+    const handleFetch = async () => {
+        try{
+            const result = await axios.get(`${URL}?query=React`);
+            setNews(result.data.hits);
+        }catch (error) {
+            console.log(error);
+        }
+    };
 
-  const handleChange = ({ target: {value} }) => {
-    setSearch(value);
-  };
+    return (
+        <div className='app'>
+            <button type="button" onClick={handleFetch}>
+                Fetch News
+            </button>
 
-  return (
-    <div className="App">
-        { user && <h2>Logged in as {user.name}</h2> }
-        <img className="image" src="" alt="search image" />
-        <Search value={search} onChange={handleChange}>
-            Search:
-        </Search>
-        <p>Searches for {search ? search : "..."}</p>
-    </div>
-  );
+            {error && <span>Something went wrong ...</span>}
+
+            <ul>
+                {news.map(({objectID, url, title}) =>(
+                    <li key={objectID}>
+                        <a href={url}>{title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
-
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import {render, screen, fireEvent, findAllByRole} from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
+import axios from "axios";
 
 // describe("App", () => {
 //   it("renders App component", () => {
@@ -27,28 +29,106 @@ import App from './App';
 //
 //   });
 // });
+//////////////////////////////////////////////////////////////////
+// describe("App", () => {
+//   test("renders App component", async () => {
+//     render(<App />);
+//     await screen.findByText(/Logged in as/i);
+//     expect(screen.queryByText(/Searches for React/)).toBeNull();
+//     fireEvent.change(screen.getByRole("textbox"), {
+//       target: {value: "React"},
+//     });
+//     screen.debug();
+//   });
+// });
+//
+// describe("events", () => {
+//   it("checkbox click", () => {
+//     const handleChange = jest.fn();
+//     const {container} = render(
+//         <input type="checkbox" onChange={handleChange}/>
+//     );
+//     const checkbox = container.firstChild;
+//     expect(checkbox).not.toBeChecked();
+//     // fireEvent.click(checkbox);
+//     userEvent.click(checkbox);
+//     // expect(handleChange).toHaveBeenCalledTimes(1);
+//     expect(checkbox).toBeChecked();
+//   });
+//
+//   it("input focus", () => {
+//     const { getByTestId } = render(
+//         <input type="text" data-testid="simple-input" />
+//     );
+//     const input = getByTestId("simple-input");
+//     expect(input).not.toHaveFocus();
+//     input.focus();
+//     expect(input).toHaveFocus();
+//   });
+//
+//   it("double click", () => {
+//     const onChange = jest.fn();
+//     const { container } = render(<input type="checkbox" onChange={onChange}/>);
+//     const checkbox = container.firstChild;
+//     expect(checkbox).not.toBeChecked();
+//     userEvent.dblClick(checkbox);
+//     expect(onChange).toHaveBeenCalledTimes();
+//   });
+//
+//   it("focus", () => {
+//     const { getAllByTestId } = render(
+//         <div>
+//           <input data-testid="element" type="checkbox"/>
+//           <input data-testid="element" type="radio"/>
+//           <input data-testid="element" type="number"/>
+//         </div>
+//     );
+//     const [checkbox, radio, number] = getAllByTestId('element');
+//     userEvent.tab();
+//     expect(checkbox).toHaveFocus();
+//   });
+//
+//   it("select option", () => {
+//       const { selectOptions, getByRole, getByText } = render(
+//           <select>
+//             <option value="1">A</option>
+//             <option value="2">B</option>
+//             <option value="3">C</option>
+//           </select>
+//       )
+//       userEvent.selectOptions(getByRole('combobox'), "1");
+//       expect(getByText("A").selected).toBeTruthy();
+//
+//       userEvent.selectOptions(getByRole('combobox'), "2");
+//       expect(getByText("B").selected).toBeTruthy();
+//       expect(getByText("A").selected).toBeFalsy();
+//   })
+// });
+//
 
+/////////////////////////////////////////////
+
+
+jest.mock("axios");
+const hits = [
+    {
+        objectID: "1",
+        title: "Angular",
+    },
+    {
+        objectID: "2",
+        title: "React",
+    },
+];
 describe("App", () => {
-  test("renders App component", async () => {
-    render(<App />);
-    await screen.findByText(/Logged in as/i);
-    expect(screen.queryByText(/Searches for React/)).toBeNull();
-    fireEvent.change(screen.getByRole("textbox"), {
-      target: {value: "React"},
-    });
-    screen.debug();
-  });
-});
+    it("fetches news from an API", async () => {
+        axios.get.mockImplementationOnce(() => Promise.resolve({data: {hits}}));
+        const { getByRole, findAllRole } = render(<App/>);
+        userEvent.click(getByRole("button"));
+        const items = await findAllByRole("listitem");
+        expect(items).toHaveLength(2);
 
-describe("events", () => {
-  it("checkbox click", () => {
-    const handleChange = jest.fn();
-    const {container} = render(
-        <input type="checkbox" onChange={handleChange}/>
-    );
-    const checkbox = container.firstChild;
-    expect(checkbox).not.toBeChecked();
-    fireEvent.click(checkbox);
-    expect(checkbox).toBeChecked();
-  })
-})
+        expect(axios.get).toHaveBeenCalledTimes(1);
+        expect(axios.get).toHaveBeenCalledWith("http://hn.algolia.com/api/v1/search?query=React");
+    });
+});
